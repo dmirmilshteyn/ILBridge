6b88f22fb8b2cbcb6ab7d924fc4644c92baab0c5
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2016-09-12 20:05:52 -0400

    Add command line parsing

--- ILBridge/Program.cs
+++ ILBridge/Program.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.CommandLineUtils;
+using System.IO;
 
 namespace ILBridge
 {
     class Program
     {
         static void Main(string[] args) {
+            var app = new CommandLineApplication();
+
+            app.Command("transpile", c =>
+            {
+                var inputAssemblyOption = c.Argument("input", "Input assembly to be transpiled", false);
+
+                c.OnExecute(() =>
+                {
+                    if (!File.Exists(inputAssemblyOption.Value)) {
+                        Console.Error.WriteLine($"Input assembly \"{inputAssemblyOption.Value}\" has not been found.");
+                        return 1;
+                    }
+
+                    return 0;
+                });
+            });
+
+            app.Execute(args);
         }
     }
 }
